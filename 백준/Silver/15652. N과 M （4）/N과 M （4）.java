7c37cc9b6484095677b417fda06d20f751e60118
userdkdk <151615343+userdkdk@users.noreply.github.com>
2024-08-13 14:41:28 +0900

    [Silver III] Title: N과 M (4), Time: 212 ms, Memory: 19548 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m; static int[] arr;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		sb = new StringBuilder();
+		getans(1,0);
+		System.out.println(sb);
+	}
+	static void getans(int num, int depth) {
+		if (depth==m) {
+			for (int j=0;j<m;j++) {
+				sb.append(arr[j]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i=num;i<=n;i++) {
+			arr[depth] = i;
+			getans(i,depth+1);
+		}
+	}
+}
