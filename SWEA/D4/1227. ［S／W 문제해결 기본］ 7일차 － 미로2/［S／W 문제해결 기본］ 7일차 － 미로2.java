75ded5f9c6d8afa43f5dfb525fa1f5dfb02fd5ce
userdkdk <151615343+userdkdk@users.noreply.github.com>
2024-10-06 15:35:46 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 143 ms, Memory: 21,048 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -0,0 +1,66 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+	static int[][] arr; static int[] sta, las; static boolean[][] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int tc=1;tc<=10;tc++) {
+			String tmp = br.readLine();
+			arr = new int[100][100];
+			visit = new boolean[100][100];
+			sta = new int[2];
+			las = new int[2];
+			for (int i=0;i<100;i++) {
+				char[] tmp_arr = br.readLine().toCharArray();
+				for (int j=0;j<100;j++) {
+					arr[i][j] = Character.getNumericValue(tmp_arr[j]);
+					if (arr[i][j]==2) {
+						sta[0] = i;
+						sta[1] = j;
+					} else if (arr[i][j]==3) {
+						las[0] = i;
+						las[1] = j;
+					}
+				}				
+			}
+			if (bfs()) {
+				System.out.println("#"+tc+" 1");
+			} else {
+				System.out.println("#"+tc+" 0");
+			}
+		}
+	}
+	static int[] dr = {1,-1,0,0}; static int[] dc = {0,0,1,-1};
+	static boolean bfs() {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(sta);
+		visit[sta[0]][sta[1]] = true;
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+			if (now[0] == las[0] && now[1] == las[1]) {
+				return true;
+			}
+			
+			for (int d=0;d<4;d++) {
+				int new_r = now[0] + dr[d];
+				int new_c = now[1] + dc[d];
+				if (visitcheck(new_r,new_c)) continue;
+				q.add(new int[] {new_r,new_c});
+				visit[new_r][new_c] = true;
+			}
+		}
+		
+		return false;
+	}
+	static boolean visitcheck(int i, int j) {
+		if (i<0 || i>=100 || j<0 || j>=100 || visit[i][j] || arr[i][j]==1) {
+			return true;
+		}
+		return false;
+	}
+}
