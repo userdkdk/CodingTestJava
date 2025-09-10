c2b2084ef600f6fd3702fd61223fa9d8145d0cb2
userdkdk <151615343+userdkdk@users.noreply.github.com>
2025-09-11 03:53:54 +0900

    [Platinum V] Title: 트리와 쿼리, Time: 1136 ms, Memory: 187336 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class Main {
     static int N, Q, K; static int[] treeP, P, counts; static int[] Sis;
-    static Map<Integer,Integer> map; static ArrayList<Integer>[] adj;
+    static boolean[] containS; static ArrayList<Integer>[] adj;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         treeP = new int[N+1];
-        Arrays.fill(treeP,-1);
         adj = new ArrayList[N+1];
         for (int i=1;i<=N;i++) adj[i] = new ArrayList<>();
         for (int i=0;i<N-1;i++) {
@@ -19,34 +19,41 @@ public class Main {
             adj[a].add(b);
             adj[b].add(a);
         }
-        makeTree(1,0);
+        visited = new boolean[N+1];
+        visited[1] = true;
+        makeTree(1);
 
         Q = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        containS = new boolean[N+1];
+
+        counts = new int[N+1];
+        Arrays.fill(counts,1);
+        P = new int[N+1];
+        for (int i=1;i<=N;i++) P[i] = i;
         for (int q=0;q<Q;q++) {
             st = new StringTokenizer(br.readLine());
             K = Integer.parseInt(st.nextToken());
-            map = new HashMap<>();
             Sis = new int[K];
-            counts = new int[K];
-            Arrays.fill(counts,1);
             for (int i=0;i<K;i++) {
                 Sis[i] = Integer.parseInt(st.nextToken());
-                map.put(Sis[i],i);
+                containS[Sis[i]] = true;
             }
-            P = new int[K];
-            Arrays.fill(P,-1);
             for (int i=0;i<K;i++) {
-                int p = getP(Sis[i]);
-                if (Sis[i]==p) continue;
-                counts[map.get(p)]++;
-            }
-            int ans = 0;
-            for (int i=0;i<K;i++) {
-                if (P[i]==-1) {
-                    int c = counts[i];
-                    ans += c*(c-1)/2;
+                int a = Sis[i];
+                if (containS[treeP[a]]) {
+                    Union(a,treeP[a]);
                 }
+            }
+            long ans = 0;
+            for (int i=0;i<K;i++) {
+                if (P[Sis[i]]==Sis[i]) {
+                    int c = counts[Sis[i]];
+                    ans += (long) c*(c-1)/2;
+                }
+                counts[Sis[i]] = 1;
+                containS[Sis[i]] = false;
+                P[Sis[i]] = Sis[i];
             }
             sb.append(ans).append("\n");
         }
@@ -54,17 +61,25 @@ public class Main {
 
     }
     static int getP(int a) {
-        int p = treeP[a];
-        if (p==-1 || map.get(p)==null) return a;
-        int a_loc = map.get(a);
-        P[a_loc] = p;
-        return getP(p);
+        if (P[a]!=a) {
+            P[a] = getP(P[a]);
+        }
+        return P[a];
     }
-    static void makeTree(int now, int p) {
+    static void Union(int a, int b) {
+        int pa = getP(a);
+        int pb = getP(b);
+        if (pa==pb) return;
+        counts[pb] += counts[pa];
+        P[pa] = pb;
+    }
+    static void makeTree(int now) {
+
         for (int next: adj[now]) {
-            if (next==p) continue;
+            if (visited[next]) continue;
             treeP[next] = now;
-            makeTree(next,now);
+            visited[next] = true;
+            makeTree(next);
         }
     }
 }
